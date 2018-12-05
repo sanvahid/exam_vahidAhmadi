e5d9388c168ef3a08d01ee68c31b338ba6840576
vahid ahmadi <san.vahid1991@gmail.com>
2018-12-05 10:33:15 +0330

    projets session 08

--- test/test.js
+++ test/test.js
@@ -1,8 +1,60 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+const chai = require('chai');
+
+let assert = chai.assert;
+let expect = chai.expect;
+
+describe('Checking session projects 8', function() {
+
+    it('sum object propertie', function() {
+      assert.equal(
+       sumObjProp (salaries = {
+        john : 200,
+        ann : 160,
+        pete : 130
+    }), 490);
     });
+
+    it('multiply numeric prop by 2', function() {
+      expect(multiplyNumeric({
+        width : 200,
+        height : 350,
+        title : 'my menu'
+      })).to.deep.equal({
+        width : 400,
+        height : 700,
+        title : 'my menu'
+      });
+    });
+
+    
   });
-});
+
+
+function sumObjProp (obj) {
+
+  let sum = 0;
+
+  for (let prop in obj) {
+
+      sum = sum + obj[prop];
+  }
+
+  return sum;
+}
+
+
+
+function multiplyNumeric (obj) {
+
+  for (let prop in obj) {
+
+      if (typeof obj[prop] === 'string') {
+          
+          continue;
+      }
+
+      obj[prop] = 2 * obj[prop];
+  }
+
+  return obj;
+}
