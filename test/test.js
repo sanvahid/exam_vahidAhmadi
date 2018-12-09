e7fe61315cbe1af3de3eba51c009e024ed532e4a
Hamed Qaderi <hamedghaderii@gmail.com>
2018-12-09 15:03:39 +0330

    Thanks no issues just syntax style problems

--- test/test.js
+++ test/test.js
@@ -4,54 +4,47 @@ let assert = chai.assert;
 let expect = chai.expect;
 
 describe('Checking calculator', function() {
-
     it('test read', function() {
-      assert.equal(calculator.read(5),5 );
+      assert.equal(calculator.read(5), 5);
     });
 
     it('test sum', function() {
-      assert.equal(calculator.sum(5),10 );
+      assert.equal(calculator.sum(5), 10);
     });
 
     it('test mul', function() {
-      assert.equal(calculator.mul(5),50 );
+      assert.equal(calculator.mul(5), 50);
     });
-
   });
 
   describe('Checking ladder', function() {
-
     it('test chaining', function() {
       assert.equal(ladder.up().up().up().down().showStep(),2 );
     });
-
   });
 
   let ladder = {
-
     step : 0,
 
     up() {
         this.step++;
+
         return this;
     },
 
     down() {
         this.step--;
+
         return this;
     },
 
     showStep() {
         return (this.step);
     }
-
- }
-
-
+ }; 
   
   
   let calculator = {
-
     number : 0,
   
     read(num) {
@@ -64,7 +57,5 @@ describe('Checking calculator', function() {
 
     mul(num) {
         return this.number = this.number * num;
-
     }
-  
   }
