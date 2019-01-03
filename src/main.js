afa8fd4ab01b7a62156d2a13818897b26ae6f495
vahid ahmadi <san.vahid1991@gmail.com>
2019-01-03 16:33:44 +0330

    exam

--- src/main.js
+++ src/main.js
@@ -1,108 +1,23 @@
 "use strict";
 
-/*
-/////////////////////////sum of two number////////////////
-
-function sumTwoNum() {
-
-  let num1 = +prompt("enter num1");
-  let num2 = +prompt("enter num2");
-
-  let result = floatFixed(num1 + num2);
-  alert(result);
-
-}
-
-sumTwoNum();
-*/
+let num = +prompt('enter a num');
 
 
-////////////////////////repeat until the input is a number//////////////////
-/*
-function readNumber() {
+function firstFactorial(num) {
 
-  let num = 0;
+  let factorialNum = 1;
+
   let i = 1;
-  let result = null;
 
-  while(!num) {
-    let num = prompt(`Enter number ${i}`);
-    
-    if (isNaN(Number(num)) || num == null || num == "") {
-      alert (`The result of your numbers is ${result}`);
-      break;
-    };
+  for (; i <= num; ) {
 
-    result = floatFixed(result + Number(num));
-
-    i++;
+      factorialNum = factorialNum * i;
+      i++;
   }
-}
 
-readNumber();
-*/
-
-
-////////////////////random float///////////////
-/*
-function random(min, max) {
-
-  let result;
-
- alert(Math.random() * (max-1) + min);
-} 
-
-random(1,5);
-*/
-
-function floatFixed(num) {
-  if (parseInt(num) === num) {
-    return num;
-  } else {
-    return +num.toFixed(2);
-  }
+  console.log(factorialNum);
+ 
 }
 
 
-///////////////////////////////check for spam////////////////////////////
-/*
-console.log(checkSpam('xx VIAgr'));
-
-function checkSpam(msg) {
-
-  msg = msg.toLowerCase();
-
-  if (msg.includes("xxx") || msg.includes("viagra")){
-    return true;
-  }else return false;
-}
-*/
-
-//////////////////////////////////////////truncate////////////////////////////
-/*
-function truncate(msg, numOfChar) {
-
-  let lengthMsg = msg.length;
-
-  if (lengthMsg > numOfChar) {
-    console.log(msg.slice(0, numOfChar) + "...");
-  }else console.log(msg);
-}
-
-truncate("jvjvjh hkh jhvj jhvj jh jhvjhv jh jvjhchgcjg  hjhv jvjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj ", 10);
-*/
-
-///////////////////////////////////////////////extract the money////////////////////////////////
-
-extractCurrencyValue('120$');
-
-function extractCurrencyValue(price) {
-
-  if (price.startsWith('$')) {
-
-    alert (price.substr(1));
-
-  }else if (price.endsWith('$')) {
-    alert (parseInt(price));
-  }else alert (price);
-}
+firstFactorial(num);
--- test/test.js
+++ test/test.js
@@ -3,112 +3,53 @@ const chai = require('chai');
 let assert = chai.assert;
 let expect = chai.expect;
 
-describe('Checking sumTwoNum', function() {
+describe('Checking longest Word', function() {
 
-    it('test sumTwoNum', function() {
-      assert.equal(sumTwoNum(0.3, 0.4), 0.7);
+    it('test longestWord', function() {
+      assert.equal(longestWord('vahid ahmadi ahmad5'), 'ahmadi');
     });
-
-    it('adds 0.1, and 0.2', function() {
-      assert.equal(sumTwoNum(0.1, 0.2), 0.3);
-    }) ;
   });
+
+  describe('Checking Factorial', function() {
+
+    it('test firstFactorial', function() {
+      assert.equal(firstFactorial(4), 24);
+    });
+  });
+
   
-
-  describe('Checking random float', function() {
-    it('test random float', function() {
-      expect(random(1, 2)).to.be.within(1,2); 
-    });
-  });
-
-  describe('Checking random integer', function() {
-    let num = randomInt(1, 5);
-    it('test random integer', function() {
-      expect(num).to.be.within(1,5); 
-    });
-    it('test if the result is Integer', function() {
-      expect(num % 1).to.be.equal(0);
-    });
-  });
-
-  describe('Checking spam', function() {
-    it('test spam', function() {
-      assert.equal(checkSpam('free viagrA'), true);
-    });
-    it ('check for xxX', function() {
-      assert.isTrue(checkSpam('Hello xxX'));
-    });
-    it('Returns false if there is no spam', () => {
-      assert.isFalse(checkSpam('Hello there.'));
-    });
-  });
-
-  // There we have a small error. extract currency should return number not a string.
-  describe('Checking extract currency value', function() {
-    it('test extractCurrencyValue', function() {
-      assert.equal(extractCurrencyValue('$120'), '120');
-    });
-    it('get number pars of 120$', () => {
-      assert.strictEqual(extractCurrencyValue('120$', 120));
-    });
-
-    it('get number pars of $120', () => {
-      assert.strictEqual(extractCurrencyValue('$120', 120));
-    });
-  });
-
-
-
-
-
 /////////////////////////////////////////functions////////////////////////////////////
 
-  function sumTwoNum(num1, num2) {
+function longestWord(string) {
 
-    // let num1 = +prompt("enter num1");
-    // let num2 = +prompt("enter num2");
-  
-    let result = floatFixed(num1 + num2);
-    return (result);
-  
+  let str = string.split(" ");
+  let longest = 0;
+  let word;
+
+  for (let i = 0; i < str.length - 1; i++) {
+
+      if (longest < str[i].length) {
+
+          longest = str[i].length;
+
+          word = str[i];
+      }
   }
-  
-  function floatFixed(num) {
-    if (parseInt(num) === num) {
-      return num;
-    } else {
-      return +num.toFixed(2);
-    }
-  }
-
-  function random(min, max) {
-  
-   return (Math.random() * (max-1) + min);
-  }
-
-  function randomInt(min, max) {
-  
-    return (Math.floor(Math.random() * (max-1) + min));
-   }
-
-
-function checkSpam(msg) {
-
-  msg = msg.toLowerCase();
-
-  if (msg.includes("xxx") || msg.includes("viagra")){
-    return true;
-  }else return false;
+  return(word);
 }
+  
+function firstFactorial(num) {
 
+  let factorialNum = 1;
 
-function extractCurrencyValue(price) {
+  let i = 1;
 
-  if (price.startsWith('$')) {
+  for (; i <= num; ) {
 
-    return (price.substr(1));
+      factorialNum = factorialNum * i;
+      i++;
+  }
 
-  }else if (price.endsWith('$')) {
-    return (parseInt(price));
-  }else return (price);
+  return factorialNum;
+ 
 }
