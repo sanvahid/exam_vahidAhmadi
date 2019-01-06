83909a80007270e7a061c48e167ace4203d37fd9
Hamed Qaderi <hamedghaderii@gmail.com>
2019-01-06 11:46:22 +0330

    debugged

--- test/test.js
+++ test/test.js
@@ -1,55 +1,60 @@
 const chai = require('chai');
 
 let assert = chai.assert;
-let expect = chai.expect;
 
-describe('Checking longest Word', function() {
-
-    it('test longestWord', function() {
-      assert.equal(longestWord('vahid ahmadi ahmad5'), 'ahmadi');
-    });
+describe('Checking longest Word', function () {
+  it('test longestWord', function () {
+    assert.equal(longestWord('vahid ahmadi ahmad5'), 'ahmadi');
   });
 
-  describe('Checking Factorial', function() {
-
-    it('test firstFactorial', function() {
-      assert.equal(firstFactorial(4), 24);
-    });
+  it('returns !', () => {
+    assert.strictEqual(longestWord('!'), '!');
   });
 
-  
-/////////////////////////////////////////functions////////////////////////////////////
+  it('returns helloooooo', () => {
+    assert.strictEqual(longestWord('This is a helloooooo'), 'helloooooo');
+  });
+});
 
-function longestWord(string) {
+describe('Checking Factorial', function () {
+  it('test firstFactorial', function () {
+    assert.equal(firstFactorial(4), 24);
+  });
 
-  let str = string.split(" ");
+  it('returns 5!', () => {
+    assert.strictEqual(firstFactorial(5), 120);
+  });
+
+  it('returns 1', () => {
+    assert.strictEqual(firstFactorial(0), 1);
+  });
+});
+
+/// //////////////////////////////////////functions////////////////////////////////////
+function longestWord (string) {
+  let str = string.split(' ');
   let longest = 0;
   let word;
 
   for (let i = 0; i < str.length - 1; i++) {
+    if (longest < str[i].length) {
+      longest = str[i].length;
 
-      if (longest < str[i].length) {
-
-          longest = str[i].length;
-
-          word = str[i];
-      }
+      word = str[i];
+    }
   }
-  return(word);
+  return (word);
 }
-  
-function firstFactorial(num) {
 
+function firstFactorial (num) {
   let factorialNum = 1;
 
   let i = 1;
 
-  for (; i <= num; ) {
-
-      factorialNum = factorialNum * i;
-      i++;
+  for (; i <= num;) {
+    factorialNum = factorialNum * i;
+    i++;
   }
 
   return factorialNum;
- 
 }
